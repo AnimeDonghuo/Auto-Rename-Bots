9cfe95a153d2f74afa05e1f8d240fab6c7692da2
AnimeDonghuo <165592897+AnimeDonghuo@users.noreply.github.com>
2025-07-23 00:48:19 +0530

    Create users_chats_db.py

---
+++ database/users_chats_db.py
@@ -0,0 +1,20 @@
+import pymongo
+from config import MONGODB_URI
+
+client = pymongo.MongoClient(MONGODB_URI)
+db = client['AutoRenameBot']
+users_collection = db['users']
+
+def add_user(user_id):
+    users_collection.update_one(
+        {"_id": user_id}, {"$set": {"upload_mode": "doc"}}, upsert=True
+    )
+
+def add_upload_mode(user_id, mode):
+    users_collection.update_one(
+        {"_id": user_id}, {"$set": {"upload_mode": mode}}, upsert=True
+    )
+
+def get_upload_mode(user_id):
+    user = users_collection.find_one({"_id": user_id})
+    return user.get("upload_mode", "doc") if user else "doc"
