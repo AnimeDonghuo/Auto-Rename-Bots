37ba66fbbc6082f6bbbf06ed5db7b38a42a429a8
AnimeDonghuo <165592897+AnimeDonghuo@users.noreply.github.com>
2025-07-23 10:45:40 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,20 +1,31 @@
-import pymongo
-from config import DB_URL
+# /database/users_chats_db.py
 
-client = pymongo.MongoClient(DB_URL)
-db = client['AutoRenameBot']
-users_collection = db['users']
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGODB_URI, DB_NAME
 
-def add_user(user_id):
-    users_collection.update_one(
-        {"_id": user_id}, {"$set": {"upload_mode": "doc"}}, upsert=True
+# MongoDB client setup
+client = AsyncIOMotorClient(MONGODB_URI)
+db = client[DB_NAME]
+
+# Collections
+users_col = db["users"]
+chat_modes_col = db["chat_modes"]
+
+# Add user to DB
+async def add_user(user_id):
+    user = await users_col.find_one({"_id": user_id})
+    if not user:
+        await users_col.insert_one({"_id": user_id})
+
+# Set upload mode (document/video)
+async def add_upload_mode(user_id, mode):
+    await chat_modes_col.update_one(
+        {"_id": user_id},
+        {"$set": {"upload_mode": mode}},
+        upsert=True
     )
 
-def add_upload_mode(user_id, mode):
-    users_collection.update_one(
-        {"_id": user_id}, {"$set": {"upload_mode": mode}}, upsert=True
-    )
-
-def get_upload_mode(user_id):
-    user = users_collection.find_one({"_id": user_id})
-    return user.get("upload_mode", "doc") if user else "doc"
+# Get upload mode
+async def get_upload_mode(user_id):
+    data = await chat_modes_col.find_one({"_id": user_id})
+    return data.get("upload_mode", "document") if data else "document"
